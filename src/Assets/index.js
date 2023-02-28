1b0a9ce92faa527426ea75d980fdd307879987d1
bora-sen <bahattinborasen@gmail.com>
2023-02-28 12:44:48 +0300

    +added assets

---
+++ src/Assets/index.js
@@ -0,0 +1,4 @@
+export {default as CheckIcon} from './icon-check.svg'
+export {default as CrossIcon} from './icon-cross.svg'
+export {default as MoonIcon} from './icon-moon.svg'
+export {default as SunIcon} from './icon-sun.svg'
